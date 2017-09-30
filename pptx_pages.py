0769b047354ade2d4ebdde5be52a905e85989106
je6bmq <>
2017-09-30 23:37:08 +0900

    implement count page number of pptx file

--- pptx_pages.py
+++ pptx_pages.py
@@ -1,8 +1,23 @@
 import tempfile
+import zipfile
+import os
+import sys
+import shutil
 
 def main():
-    dirname=tempfile.mkdtemp(dir="./")
-    print(dirname)
+    args=sys.argv
+    dir_path=tempfile.mkdtemp(dir="./")
+    pptx_file=os.path.join(args[1])
+    
+    with zipfile.ZipFile(pptx_file,"r") as z_file:
+        z_file.extractall(path=dir_path)
+
+    slides_path=os.path.join(dir_path,"ppt","slides")
+    slides_xmls=[i for i in os.listdir(slides_path) if os.path.isfile(os.path.join(slides_path,i))]
+    
+    print(len(slides_xmls))
+
+    shutil.rmtree(dir_path)
 
 if __name__ == '__main__':
     main()
