ad1e50b20df1cfc3fb6664d6b2bddc2af291717a
je6bmq <>
2017-09-30 23:52:06 +0900

    implement for multiple inputs

--- pptx_pages.py
+++ pptx_pages.py
@@ -10,18 +10,20 @@ def main():
         print("please input pptx file..")
         sys.exit(1)
     
-    dir_path=tempfile.mkdtemp(dir="./")
-    pptx_file=os.path.join(args[1])
-    
-    with zipfile.ZipFile(pptx_file,"r") as z_file:
-        z_file.extractall(path=dir_path)
+    for arg in args[1:]:
 
-    slides_path=os.path.join(dir_path,"ppt","slides")
-    slides_xmls=[i for i in os.listdir(slides_path) if os.path.isfile(os.path.join(slides_path,i))]
-    
-    print(len(slides_xmls))
+        dir_path=tempfile.mkdtemp(dir="./")
+        pptx_file=os.path.join(arg)
+        
+        with zipfile.ZipFile(pptx_file,"r") as z_file:
+            z_file.extractall(path=dir_path)
 
-    shutil.rmtree(dir_path)
+        slides_path=os.path.join(dir_path,"ppt","slides")
+        slides_xmls=[i for i in os.listdir(slides_path) if os.path.isfile(os.path.join(slides_path,i))]
+        
+        print(arg+", "+str(len(slides_xmls))+" pages")
+
+        shutil.rmtree(dir_path)
 
 if __name__ == '__main__':
     main()
