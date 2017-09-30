da721860152d2c49f7c34d3d0ce83548f717e135
je6bmq <>
2017-10-01 00:16:25 +0900

    catch some exception..

--- pptx_pages.py
+++ pptx_pages.py
@@ -17,13 +17,17 @@ def main():
             dir_path=tempfile.mkdtemp(dir="./")
             pptx_file=os.path.join(arg)
             
-            with zipfile.ZipFile(pptx_file,"r") as z_file:
-                z_file.extractall(path=dir_path)
-
-            slides_path=os.path.join(dir_path,"ppt","slides")
-            slides_xmls=[i for i in os.listdir(slides_path) if os.path.isfile(os.path.join(slides_path,i))]
-            
-            print(arg+", "+str(len(slides_xmls))+" pages")
+            try:
+                with zipfile.ZipFile(pptx_file,"r") as z_file:
+                    z_file.extractall(path=dir_path)                
+            except FileNotFoundError as e:
+                print(arg+ " is not found..")
+            except zipfile.BadZipFile as e:
+                print(arg+ " is not correct pptx file..")
+            else:
+                slides_path=os.path.join(dir_path,"ppt","slides")
+                slides_xmls=[i for i in os.listdir(slides_path) if os.path.isfile(os.path.join(slides_path,i))]
+                print(arg+", "+str(len(slides_xmls))+" pages")
 
             shutil.rmtree(dir_path)
         else:
